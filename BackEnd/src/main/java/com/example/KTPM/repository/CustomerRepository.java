1139417368d52b48da31af8f1db24a4bfda30ea0
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2025-05-27 22:23:26 +0700

    delete customer

--- BackEnd/src/main/java/com/example/KTPM/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.KTPM.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Optional<Customer> findByEmailIgnoreCase(String email);
-}
